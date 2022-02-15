da5a454cf89d18c22cdccf0ec609259fb11cd8ba
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-15 18:20:37 +0100

    Small fix to the delete confirmation alert

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -44,6 +44,7 @@ function Delete(url) {
         title: "Are you sure?",
         text: "Once deleted, you will not be able to recover",
         icon: "warning",
+        buttons: true,
         dangerMode: true
     }).then((willDelete) => {
         if (willDelete) {
