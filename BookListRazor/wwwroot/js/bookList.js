6d3c53e897fd918982f038f93f8c42ac93d1836f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-16 16:32:20 +0100

    Added upsert (insert/update) functionality to add/modify a book

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                         <div class="text-center">
-                            <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
+                            <a href="/BookList/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
                                 Edit
                             </a>
                             &nbsp;
