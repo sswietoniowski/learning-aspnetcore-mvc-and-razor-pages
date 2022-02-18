090249a28c62e3274c9c1a465b3d5441dd6a8ecd
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-18 16:03:44 +0100

    Added category controller with code scaffolding
    Only for tests!

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -8,9 +8,24 @@ namespace BulkyBook.DataAccess.Data
     {
         public DbSet<Category> Categories { get; set; }
 
+        public ApplicationDbContext()
+        {
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // added only to solve problem with controllers scaffolding
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BulkyBookMVC;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
