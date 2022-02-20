79011c94c9fb3055423d8eb8ca4cfe20f2d7b78c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-20 09:49:07 +0100

    Added stored procedures for the CoverType

---
+++ BulkyBook.DataAccess/Data/Migrations/20220220084739_AddStoredProceduresForCoverType.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class AddStoredProceduresForCoverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"
+CREATE PROCEDURE dbo.usp_CoverTypes_SelectAll
+AS
+BEGIN
+	SELECT Id, Name
+	FROM dbo.CoverTypes;
+END;
+"
+            );
+
+            migrationBuilder.Sql(
+@"
+CREATE PROCEDURE dbo.usp_CoverTypes_Select
+	@Id int
+AS
+BEGIN
+	SELECT Id, Name
+	FROM dbo.CoverTypes
+	WHERE Id = @Id;
+END;
+"
+            );
+
+            migrationBuilder.Sql(
+@"
+CREATE PROCEDURE dbo.usp_CoverTypes_Insert
+	@Name nvarchar(32)
+AS
+BEGIN
+	INSERT INTO dbo.CoverTypes (Name)
+	VALUES (@Name)
+	SELECT SCOPE_IDENTITY();
+END;
+"
+            );
+
+            migrationBuilder.Sql(
+@"
+CREATE PROCEDURE dbo.usp_CoverTypes_Update
+	@Id int,
+	@Name nvarchar(32)
+AS
+BEGIN
+	UPDATE dbo.CoverTypes
+	SET Name = @Name
+	WHERE Id = @Id;
+END;
+"
+            );
+
+            migrationBuilder.Sql(
+@"
+CREATE PROCEDURE dbo.usp_CoverTypes_Delete
+	@Id int
+AS
+BEGIN
+	DELETE FROM dbo.CoverTypes
+	WHERE Id = @Id;
+END;
+"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE dbo.usp_CoverTypes_SelectAll");
+            migrationBuilder.Sql(@"DROP PROCEDURE dbo.usp_CoverTypes_Select");
+            migrationBuilder.Sql(@"DROP PROCEDURE dbo.usp_CoverTypes_Insert");
+            migrationBuilder.Sql(@"DROP PROCEDURE dbo.usp_CoverTypes_Update");
+            migrationBuilder.Sql(@"DROP PROCEDURE dbo.usp_CoverTypes_Delete");
+        }
+    }
+}
