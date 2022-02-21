c11c3f89744816e5ce1cfc68991c1be770756963
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-21 14:39:08 +0100

    Added scaffolded pages for the Identity

---
+++ BulkyBookMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BulkyBook.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BulkyBook.MVC.Areas.Identity.IdentityHostingStartup))]
+namespace BulkyBook.MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
