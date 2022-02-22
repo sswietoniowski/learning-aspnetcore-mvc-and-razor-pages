87ac42aa8f8cfa697ccf72623880244a2a2d7a10
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-22 08:24:44 +0100

    Added user's list display

---
+++ BulkyBookMVC/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace BulkyBook.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            // we could use a UoW (as in the other controllers), this time we're using DbContext, just
+            // to show an alternate way of doing things; normally we wouldn't mix the two approaches,
+            // now it is only for the demo purposes
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            users.ForEach(u =>
+            {
+                var roleId = userRoles.FirstOrDefault(ur => u.Id == ur.UserId).RoleId;
+                u.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+                if (u.Company is null)
+                {
+                    u.Company = new Company { Name = "" };
+                }
+            });
+
+            return Json(new { data = users });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyBookMVC/wwwroot/js/users.js
@@ -0,0 +1,60 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url": "/Admin/Users/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+        //    {
+        //        "data": "id",
+        //        "render": function(data) {
+        //            return `
+        //                <div class="text-center">
+        //                    <a class="btn btn-danger text-white fas fa-trash-alt"
+        //                        onclick=Delete('/Admin/Users/Delete/${data}')>
+        //                        <i style="cursor:pointer"></i>
+        //                    </a>
+        //                </div>
+        //            `
+        //        },
+        //        "width": "25%"
+        //    }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
