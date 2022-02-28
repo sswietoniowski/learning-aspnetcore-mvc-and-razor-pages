1e4103621519225107a65715bc53c3d851e79bc9
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-28 18:14:53 +0100

    Added ShoppingCart

---
+++ BulkyBook.Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(ApplicationUserId))]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [Range(1, 1000, ErrorMessage ="Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        [NotMapped]
+        public decimal Price { get; set; }
+    }
+}
