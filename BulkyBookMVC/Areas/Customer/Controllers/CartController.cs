8f099c1d59f9334c5e92907781f29f48480e98e4
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-03-05 19:53:11 +0100

    Added shopping cart controller

---
+++ BulkyBookMVC/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBook.MVC.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                OrderHeader = new OrderHeader(),
+                ListCart = _unitOfWork.ShoppingCarts.GetAll(
+                    u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
+            };
+            ShoppingCartViewModel.OrderHeader.OrderTotal = 0;
+            ShoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUsers
+                .GetFirstOrDefault(u => u.Id == claim.Value, includeProperties: "Company");
+
+            foreach (var list in ShoppingCartViewModel.ListCart)
+            {
+                list.Price = SD.GetPriceBasedOnQuantity(list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+                ShoppingCartViewModel.OrderHeader.OrderTotal += (list.Price * list.Count);
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+                if (list.Product.Description.Length > 100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            return View(ShoppingCartViewModel);
+        }
+    }
+}
