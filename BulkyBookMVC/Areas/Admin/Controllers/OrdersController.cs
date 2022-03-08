458b571127a7503b6112e0f37992794ca23d34b4
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-03-08 08:35:45 +0100

    Added orders view

---
+++ BulkyBookMVC/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,73 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace BulkyBook.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrdersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<OrderHeader> orderHeaders;
+
+            if (User.IsInRole(SD.IdentityRole_Admin) || User.IsInRole(SD.IdentityRole_Employee))
+            {
+                orderHeaders = _unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaders = _unitOfWork.OrderHeaders
+                    .GetAll(o => o.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "Processing":
+                    orderHeaders = orderHeaders.Where(oh => oh.OrderStatus == SD.OrderStatus_InProcess ||
+                        oh.OrderStatus == SD.OrderStatus_Approved || oh.OrderStatus == SD.OrderStatus_Pending);
+                    break;
+                case "Pending":
+                    orderHeaders = orderHeaders.Where(oh => oh.PaymentStatus == SD.PaymentStatus_DelayedPayment);
+                    break;
+                case "Completed":
+                    orderHeaders = orderHeaders.Where(oh => oh.OrderStatus == SD.OrderStatus_Shipped);
+                    break;
+                case "Rejected":
+                    orderHeaders = orderHeaders.Where(oh => oh.OrderStatus == SD.OrderStatus_Cancelled ||
+                        oh.OrderStatus == SD.OrderStatus_Refunded || oh.PaymentStatus == SD.PaymentStatus_Rejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyBookMVC/wwwroot/js/orders.js
@@ -0,0 +1,45 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("Processing")) {
+        loadDataTable("GetAll?status=Processing");
+    } else if (url.includes("Pending")) {
+        loadDataTable("GetAll?status=Pending");
+    } else if (url.includes("Completed")) {
+        loadDataTable("GetAll?status=Completed");
+    } else if (url.includes("Rejected")) {
+        loadDataTable("GetAll?status=Rejected");
+    } else {
+        loadDataTable("GetAll?status=All");
+    }
+})
+
+function loadDataTable(url) {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url": "/Admin/Orders/" + url
+        },
+        "columns": [
+            { "data": "id", "width": "10%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function(data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Orders/Details/${data}" class="btn btn-success text-white fas fa-edit">
+                                <i style="cursor:pointer"></i>
+                            </a>
+                        </div>
+                    `
+                },
+                "width": "40%"
+            }
+        ]
+    });
+}
