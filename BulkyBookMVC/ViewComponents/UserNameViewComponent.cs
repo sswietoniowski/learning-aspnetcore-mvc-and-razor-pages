4462a96cfe87ba2f73d41cb4baa88daf3039f2b9
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-03-24 10:55:25 +0100

    Created custom view component

---
+++ BulkyBookMVC/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BulkyBook.MVC.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);   
+            var userFromDb = _unitOfWork.ApplicationUsers.GetFirstOrDefault(u => u.Id == claims.Value);
+
+            return View(userFromDb);
+        }
+    }
+}
